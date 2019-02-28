4a75a3020aa4c26c4454f70d99d4894711466b8f
Co-Din <din.stolak@proagrica.com>
2019-02-28 11:30:25 +0000

    Introduced a better reporting system for tests, which breaks down time, scenarios and tests...

--- src/test/java/com/adaptris/runner/TestRunner.java
+++ src/test/java/com/adaptris/runner/TestRunner.java
@@ -1,18 +1,22 @@
 package com.adaptris.runner;
 
+import com.cucumber.listener.Reporter;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.testng.annotations.AfterClass;
+
+import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         tags = "@FunctionalTest",
         features = "src/test/resources/com/adaptris/features/",
-        glue = {"com/adaptris/stepdefs/"},
-        plugin = { "pretty", "html:target/htmlreports" }
+        glue = { "com/adaptris/stepdefs/" },
+        plugin = { "json:target/htmlreports/cucumber.json" }
 )
 
-public class testrunner {
+public class TestRunner {
 
 }
 
