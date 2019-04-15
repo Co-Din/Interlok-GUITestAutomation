00d4d9196f3f3bcd3b66aa67b6c2007834072f61
Co-Din <din.stolak@proagrica.com>
2019-04-15 16:02:51 +0100

    Asserts Channel workflows.

--- src/test/java/com/adaptris/stepdefs/StepDefinitions.java
+++ src/test/java/com/adaptris/stepdefs/StepDefinitions.java
@@ -2373,12 +2373,6 @@ public class StepDefinitions {
 
   }
 
-  ///html/body/section/div[2]/section[3]/div[1]/div/div[4]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[1]
-  ///html/body/section/div[2]/section[3]/div[1]/div/div[4]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[2]
-  ///html/body/section/div[2]/section[3]/div[1]/div/div[4]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[3]
-  ///html/body/section/div[2]/section[3]/div[1]/div/div[4]/div/div/div[1]/div[4]/div/div[2]/div[1]/div[1]
-  ///html/body/section/div[2]/section[3]/div[1]/div/div[4]/div/div/div[1]/div[4]/div/div[2]/div[1]/div[2]
-
 //*******************************************************************************************
 //*******************************************************************************************
 
